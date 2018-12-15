3799786da7e074fde980ed615b34d49c5d3c79c4
Hazrat Ali <iloveyii@yahoo.com>
2018-12-16 00:28:39 +0100

    Added migration and test for User

--- backend/src/init.php
+++ backend/src/init.php
@@ -1,18 +1,27 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once 'config/app.php';
+    require_once 'vendor/autoload.php';
+    require_once 'config/app.php';
 
-use App\Models\Database;
+    // Clear previous log files if any
+    \App\Models\Log::clear();
 
-// Clear previous log files if any
-\App\Models\Log::clear();
+    /**
+     * Create table task
+     */
+    $task = new \App\Models\Task();
+    $task->dropTable();
+    $task->createTable();
+    printf("Created table %s. " . PHP_EOL, $task->tableName);
+    echo PHP_EOL;
 
-/**
- * Create table task
- */
-$task = new \App\Models\Task();
-$task->dropTable();
-$task->createTable();
-printf ("Created table %s. " . PHP_EOL, $task->tableName);
-echo PHP_EOL;
-
+    /**
+     * Create table user and insert default user admin with pass admin
+     */
+    $user = new \App\Models\User();
+    $user->dropTable();
+    $user->createTable();
+    printf("Created table %s. " . PHP_EOL, $user->tableName);
+    $attributes = ['username' => 'admin', 'password' => 'admin'];
+    $user->setAttributes($attributes)->create();
+    printf("Inserted admin user into table %s. " . PHP_EOL, $user->tableName);
+    echo PHP_EOL;
--- backend/src/tests/unit/UserTest.php
+++ backend/src/tests/unit/UserTest.php
@@ -6,10 +6,67 @@
 
     class UserTest extends TestCase
     {
-        public $task;
+        public $user;
 
         public function setUp()
         {
             $this->user = new User();
         }
+
+        public function testUserTableReturnsArray()
+        {
+            $rows = $this->user->read();
+            $this->assertInstanceOf('App\Models\User', $this->user);
+            $this->assertTrue(is_array($rows));
+        }
+
+        /*
+        public function testCanInsertDataIntoUser()
+        {
+            $data = [
+                'title' => 'Develop Todo list',
+                'status' => 'IN_PROGRESS'
+            ];
+            $this->user->setAttributes($data);
+            $id = $this->user->create();
+            $this->assertTrue($id > 0);
+        }
+
+        public function testCanDeleteDataFromUser()
+        {
+            $data = [
+                'title' => 'Develop Todo list 2',
+                'status' => 'IN_PROGRESS'
+            ];
+            $this->user->setAttributes($data);
+            $this->user->create();
+            $result = $this->user->delete();
+            $this->assertTrue($result > 0);
+        }
+
+        public function testCanUpdateDataIntoUser()
+        {
+            $data = [
+                'title' => 'Develop Todo list 3',
+                'status' => 'IN_PROGRESS'
+            ];
+            $this->user->setAttributes($data);
+            $this->user->create();
+            // Fetch record by id
+            $row = $this->user->read($this->user->id);
+            $this->assertEquals('Develop Todo list 3', $row['title']);
+
+            // Update
+            $data = [
+                'id' => $this->user->id,
+                'title' => 'Develop Todo list 4',
+                'status' => 'DONE'
+            ];
+            $this->user->setAttributes($data);
+            $this->user->update();
+            $row = $this->user->read($this->user->id);
+            $this->assertEquals('Develop Todo list 4', $row['title']);
+        }
+        */
+
     }
